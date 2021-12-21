6c90ebe2c45e3d95c49e6a378f2297cb2dbe0e3e
Luis Sousa <luysd2@gmail.com>
2021-12-21 16:38:48 -0300

    Usando a classe Usuario

--- 13BD-PDO-eDAO/aula64/index.php
+++ 13BD-PDO-eDAO/aula64/index.php
@@ -1,6 +1,10 @@
 <?php
 require_once("config.php");
-$sql = new Sql();
-$usuario =$sql->select("SELECT * FROM tb_usuario");
-echo json_encode($usuario);
+// $sql = new Sql();
+// $usuario =$sql->select("SELECT * FROM tb_usuario");
+// echo json_encode($usuario);
+
+$root = new Usuario();
+$root->loadById(6);
+echo $root;
 ?>
