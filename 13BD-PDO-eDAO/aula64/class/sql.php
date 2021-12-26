da5a83a4d24a701ac3d7a4c0d3e7fba0dfc2d682
Luis Sousa <luysd2@gmail.com>
2021-12-26 12:52:00 -0300

    Modifiquei alguns nomes de variaveis

--- 13BD-PDO-eDAO/aula64/class/sql.php
+++ 13BD-PDO-eDAO/aula64/class/sql.php
@@ -9,26 +9,26 @@ class Sql extends PDO{
         $this->conn = new PDO("mysql:host=Localhost;dbname=dbphp7", "root", "");
     }
 
-    private function setParams($statment, $parameers = array()){
-        foreach($parameers as $kay => $value){
-            $this -> setParam($statment, $kay, $value);
+    private function setParams($statement, $parameters = array()){
+        foreach($parameters as $key => $value){
+            $this -> setParam($statement, $key, $value);
             /* o erro é que o setParam so tinha dois paramentros($kay, $value) 
             e tinha que ser 3 ($statment, $kay, $value) */
         }
     }
-    private function setParam($statment, $key, $value){
-        $statment->bindParam($key, $value);
+    private function setParam($statement, $key, $value){
+        $statement->bindParam($key, $value);
     }
     // ele mostra esses "erros" mas o codigo funcina 
-    public function query($rowQuery, $params = array()){
-        $stmt = $this->conn->prepare($rowQuery);
+    public function query($rawQuery, $params = array()){
+        $stmt = $this->conn->prepare($rawQuery);
         $this->setParams($stmt, $params);
         $stmt->execute();
         return $stmt;
     }
 
-    public function select($rowQuery, $params= array()): array{
-        $stmt = $this->query($rowQuery, $params);
+    public function select($rawQuery, $params= array()): array{
+        $stmt = $this->query($rawQuery, $params);
         return $stmt -> fetchAll(PDO::FETCH_ASSOC);
     }
 }
