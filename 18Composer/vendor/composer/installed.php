39197d86ae5c2960330e25edb6fbbec0cb812874
Luis Sousa <luysd2@gmail.com>
2022-01-07 11:12:11 -0300

    Gerenciando dependências do composer

---
+++ 18Composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2b68759bf827919ec508316d7cebc2a61666dcf4',
+        'name' => 'luysd2/composer',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'luysd2/composer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2b68759bf827919ec508316d7cebc2a61666dcf4',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.0.7',
+            'version' => '6.0.7.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => '0c41a36d4508d470e376498c1c0c527aa36a2d59',
+            'dev_requirement' => false,
+        ),
+    ),
+);
