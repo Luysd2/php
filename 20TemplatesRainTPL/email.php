6d6b1ae36b93ae686aacb26f88d0139fad48cfe8
Luis Sousa <luysd2@gmail.com>
2022-01-07 22:07:33 -0300

    Trabalhando com templates usando RainTPL

---
+++ 20TemplatesRainTPL/email.php
@@ -0,0 +1,105 @@
+<?php
+require __DIR__ . "/vendor/autoload.php";
+
+// namespace
+use Rain\Tpl;
+
+
+// config
+$config = array(
+    "tpl_dir"       => "tpl/",
+    "cache_dir"     => "cache/",
+);
+
+Tpl::configure( $config );
+
+// create the Tpl object
+$tpl = new Tpl;
+
+// assign a variable
+$tpl->assign( "name", "Luis Sousa" );
+$tpl->assign( "version", PHP_VERSION );
+
+// assign an array
+//$tpl->assign( "week", array( "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ) );
+
+// draw the template
+$html = $tpl->draw( "index", true);
+
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+
+//Enable SMTP debugging
+//SMTP::DEBUG_OFF = off (for production use)
+//SMTP::DEBUG_CLIENT = client messages
+//SMTP::DEBUG_SERVER = client and server messages
+$mail->SMTPDebug = 2;
+
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+//Use `$mail->Host = gethostbyname('smtp.gmail.com');`
+//if your network does not support SMTP over IPv6,
+//though this may cause issues with TLS
+
+//Set the SMTP port number:
+// - 465 for SMTP with implicit TLS, a.k.a. RFC8314 SMTPS or
+// - 587 for SMTP+STARTTLS
+$mail->Port = 587;
+
+//Set the encryption mechanism to use:
+// - SMTPS (implicit TLS on port 465) or
+// - STARTTLS (explicit TLS on port 587)
+$mail->SMTPSecure = 'tls';
+
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = 'exemple@gmail.com';
+
+//Password to use for SMTP authentication
+$mail->Password = 'xxxxxxxx';
+
+//Set who the message is to be sent from
+//Note that with gmail you can only use your account address (same as `Username`)
+//or predefined aliases that you have configured within your account.
+//Do not use user-submitted addresses in here
+$mail->setFrom('exemple@gmail.com', 'Luis');
+
+//Set an alternative reply-to address
+//This is a good place to put user-submitted addresses
+//$mail->addReplyTo('replyto@example.com', 'First Last');
+
+//Set who the message is to be sent to
+$mail->addAddress('exemple@gmail.com', 'Luis Fernando');
+
+//Set the subject line
+$mail->Subject = 'Testando o PHPMailer index.php';
+
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML($html);
+
+//Replace the plain text body with one created manually
+$mail->AltBody = 'Teste de PHPMailer';
+
+//Attach an image file
+//$mail->addAttachment('images/phpmailer_mini.png');
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message sent!';
+    //Section 2: IMAP
+    //Uncomment these to save your message in the 'Sent Mail' folder.
+    #if (save_mail($mail)) {
+    #    echo "Message saved!";
+    #}
+}
+
+?>
